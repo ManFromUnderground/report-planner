af65ea43e424599cca647769f799034e8e4a93dc
ManFromUnderground <98041206+ManFromUnderground@users.noreply.github.com>
2022-04-12 01:33:57 -0400

    Update simple_planner3.py

--- simple_planner3.py
+++ simple_planner3.py
@@ -15,16 +15,26 @@ from tf.transformations import *
 from geometry_msgs.msg import Quaternion
 
 ball = SphereParams()
+#based_ball = tf2_geometry_msgs.PointStamped()
 
 def get_ball(param):
-	#print("ball found")
 	global ball
+	#global based_ball
+	point1 = tf2_geometry_msgs.PointStamped()
 	ball.xc = param.xc
 	ball.yc = param.yc
 	ball.zc = param.zc
 	ball.radius = param.radius
-
-
+	"""if param.xc != 0.0 and param.yc != 0.0 and param.zc != 0.0:
+		point1.point.x = param.xc
+		point1.point.y = param.yc
+		point1.point.z = param.zc
+		point1.header.stamp = 'camera_color_optical'
+	buffer2 = tf2_ros.Buffer()
+	listener2 = tf2_ros.TransformListener(buffer2)
+	if param.xc != 0.0 and param.yc != 0.0 and param.zc != 0.0:
+		based_ball = buffer2.transform(point1,'base',rospy.Duration(1.0))"""
+	
 
 if __name__ == '__main__':
 	# initialize the node
@@ -35,24 +45,35 @@ if __name__ == '__main__':
 	loop_rate = rospy.Rate(10)
 	#sub to filtered parameters
 	
+	not_except = False
+	
 	ball_sub = rospy.Subscriber('/sphere_params', SphereParams, get_ball)
 	
 	buffer = tf2_ros.Buffer()
 	listener = tf2_ros.TransformListener(buffer)
-	
+	print("listener:", listener)
 	qrot = Quaternion()
 	
 	
 	cam = tf2_geometry_msgs.PointStamped()
 	
+	base = tf2_geometry_msgs.PointStamped()
 	#cam.x, cam.y, cam.z, cam.rotation = None, None, None, None
 	
-	while cam.point.x is None or cam.point.y is None or cam.point.z is None:
+	while ((cam.point.x is None or ball.xc == 0.0) or (cam.point.y is None or ball.yc == 0.0) or (cam.point.z is None or ball.zc == 0.0)) or not_except == True:
 		try:
 			trans = buffer.lookup_transform("base", "camera_color_optical", rospy.Time())
+			print("trans is:", trans)
+			not_except = True
 		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
 			print('frames unavailable')
-			loop_rate.sleep()
+			print("cam x:", cam.point.x)
+			print("cam y:", cam.point.y)
+			print("cam z:", cam.point.z)
+			print("ball x:", ball.xc)
+			print("ball y:", ball.yc)
+			print("ball z:", ball.zc)
+			
 			continue
 		"""x = trans.transform.translation.x
 		y = trans.transform.translation.y
@@ -60,20 +81,20 @@ if __name__ == '__main__':
 		
 		"""qrot = trans.transform.rotation
 		roll, pitch, yaw, = euler_from_quaternion([qrot.x, qrot.y, qrot.z, qrot.w])"""
-		
-		
-		cam.header.frame_id = 'camera_color_optical'
+		cam.header.frame_id = "camera_color_optical"
 		cam.header.stamp = rospy.get_rostime()
+		#while ball.xc != cam.point.x:
 		cam.point.x = ball.xc
 		cam.point.y = ball.yc
 		cam.point.z = ball.zc
-		print(cam)
+			
+		print("attempted to assign")
 		
 		base = buffer.transform(cam, 'base', rospy.Duration(1.0))
 		
 		qrot = cam.transform.rotation
 		roll, pitch, yaw, = euler_from_quaternion([qrot.x, qrot.y, qrot.z, qrot.w])
-		
+		loop_rate.sleep()
 		
 	# define a plan variable
 	plan = Plan()
@@ -82,20 +103,21 @@ if __name__ == '__main__':
 	
 	
 	# above ball
-	plan_point0.linear.x = cam.point.x
-	plan_point0.linear.y = cam.point.y
-	plan_point0.linear.z = cam.point.z + 0.5
+	plan_point0.linear.x = base.point.x
+	plan_point0.linear.y = base.point.y
+	plan_point0.linear.z = base.point.z + 0.5
 	plan_point0.angular.x = 1.57
 	plan_point0.angular.y = 0.0
 	plan_point0.angular.z = 0.0
 	# add this point to the plan
 	plan.points.append(plan_point0)
 	
+	print("plan 0:", plan_point0)
 	
 	plan_point1 = Twist()
-	plan_point1.linear.x = cam.point.x
-	plan_point1.linear.y = cam.point.y
-	plan_point1.linear.z = cam.point.z
+	plan_point1.linear.x = base.point.x
+	plan_point1.linear.y = base.point.y
+	plan_point1.linear.z = base.point.z
 	plan_point1.angular.x = 1.57
 	plan_point1.angular.y = 0.0
 	plan_point1.angular.z = 0.0
@@ -138,7 +160,7 @@ if __name__ == '__main__':
 	plan.points.append(plan_point4)
 
 	
-	
+	print("reached last while")
 	while not rospy.is_shutdown():
 		
 		
